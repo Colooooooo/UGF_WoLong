a930f791e6956993468e907b8f2396596215eae7
passion <yupu@gillar.com>
2023-04-12 09:40:33 +0800

    回滚procedure

--- Assets/GameMain/Scripts/Hotfix/Base/GameEntry.cs
+++ Assets/GameMain/Scripts/Hotfix/Base/GameEntry.cs
@@ -32,7 +32,41 @@ namespace StarForce
         
         private static void ResetProcedure() 
         {
-            Procedure.ResetProcedure(m_EntranceProcedureTypeName);
+            //卸载流程
+            Fsm.DestroyFsm<GameFramework.Procedure.IProcedureManager>();
+            GameFramework.Procedure.IProcedureManager procedureManager = GameFramework.GameFrameworkEntry.GetModule<GameFramework.Procedure.IProcedureManager>();
+            //创建新的流程 HotfixFramework.Runtime
+            string[] m_ProcedureTypeNames = TypeUtils.GetRuntimeTypeNames(typeof(ProcedureBase), m_HotfixAssemblys);
+            ProcedureBase[] procedures = new ProcedureBase[m_ProcedureTypeNames.Length];
+            for (int i = 0; i < m_ProcedureTypeNames.Length; i++)
+            {
+                Type procedureType = GameFramework.Utility.Assembly.GetType(m_ProcedureTypeNames[i]);
+                if (procedureType == null)
+                {
+                    Log.Error("Can not find procedure type '{0}'.", m_ProcedureTypeNames[i]);
+                    return;
+                }
+
+                procedures[i] = (ProcedureBase)Activator.CreateInstance(procedureType);
+                if (procedures[i] == null)
+                {
+                    Log.Error("Can not create procedure instance '{0}'.", m_ProcedureTypeNames[i]);
+                    return;
+                }
+
+                if (m_EntranceProcedureTypeName == m_ProcedureTypeNames[i])
+                {
+                    m_EntranceProcedureBase = procedures[i];
+                }
+            }
+
+            if (m_EntranceProcedureBase == null)
+            {
+                Log.Error("Entrance procedure is invalid.");
+                return;
+            }
+            procedureManager.Initialize(GameFramework.GameFrameworkEntry.GetModule<GameFramework.Fsm.IFsmManager>(), procedures);
+            procedureManager.StartProcedure(m_EntranceProcedureBase.GetType());
         }
     }
 }
